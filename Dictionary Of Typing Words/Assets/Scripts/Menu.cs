015670008d90831a4c501dda3766bfd159c693eb
EthanCooper20 <escoope77@gmail.com>
2021-04-01 21:09:12 -0500

    Added Pause Menu and Player Input Name

---
+++ Dictionary Of Typing Words/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public InputField playername;
+
+    public void StartGame(string input)
+    {
+        Debug.Log("Player Name is:" + playername.text);
+        MySettings.playernamestr = playername.text;
+        SceneManager.LoadScene("Main");
+    }
+
+}
---
+++ Dictionary Of Typing Words/Assets/Scripts/MySettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MySettings : MonoBehaviour
+{
+    public static string playernamestr;
+
+
+    public Text playername;
+
+    void Start()
+    {
+        playername.text = playernamestr;
+    }
+
+}
